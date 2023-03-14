8d5f58bd61b3535095237190098cc7eedc55e314
s52108 <s52108@fh.campus02.at>
2023-03-14 09:05:16 +0100

    Initial commit & Added Example Instructions

---
+++ src/online/firstLesson/IncomeTaxCalculationAustriaApp.java
@@ -0,0 +1,23 @@
+package online.firstLesson;
+
+public class IncomeTaxCalculationAustriaApp {
+
+
+    /*
+                 < 11.000 €       -> Steuerfrei
+    > 11.000 € & < 18.000 €       -> 20%
+    < 18.000 € & < 31.000 €       -> 32.5%
+    > 31.000 € & < 60.000 €       -> 42%
+    > 60.000 € & < 90.000 €       -> 48%
+    > 90.000 € & < 1.000.000 €    -> 50%
+    >1.000.000                    -> 55%
+
+    SV Beitrag: 18,12%
+
+
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+}
