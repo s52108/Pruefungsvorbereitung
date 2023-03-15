2851fdad62e7d7cba18309546bbed1d7f445427f
s52108 <s52108@fh.campus02.at>
2023-03-15 10:48:02 +0100

    Added VariableDemo

---
+++ src/Uebungslatt_LV1/VariableDemo.java
@@ -0,0 +1,10 @@
+package Uebungslatt_LV1;
+
+public class VariableDemo {
+    public static void main(String[] args) {
+        String vorname = new String("Adolf Simon");
+        String nachname = new String("Postl");
+
+        System.out.println("Der Name lautet: " + vorname + " " + nachname);
+    }
+}
